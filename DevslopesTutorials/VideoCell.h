307e2fecb24260ba28fdb7ed6557748bece9d5e1
mshayanh13 <mohammadhemani@gmail.com>
2017-04-07 07:03:42 -0500

    custom tableviewcell

---
+++ DevslopesTutorials/VideoCell.h
@@ -0,0 +1,13 @@
+//
+//  VideoCell.h
+//  DevslopesTutorials
+//
+//  Created by Mohammad Hemani on 4/7/17.
+//  Copyright © 2017 Mohammad Hemani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoCell : UITableViewCell
+
+@end
